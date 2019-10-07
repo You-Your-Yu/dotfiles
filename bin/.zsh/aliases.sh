83cbe82788a0fd175c44e9287969d616a8a88af9
Yu <ya4ma1da3@gmail.com>
2019-10-07 21:31:00 +0900

    alias of nvidia-smi

--- bin/.zsh/aliases.sh
+++ bin/.zsh/aliases.sh
@@ -8,6 +8,9 @@ alias gp='git push'
 # tmux
 alias ta='tmux a'
 
+# gpu
+alias ns='nvidia-smi'
+
 # fujiso-san configuration↲
 alias gpu='watch -n1 "hostname; nvidia-smi pmon -s um -c 1"'↲
 alias imux='tmux attach || tmux new-session \; source-file ~/.tmux/imux'
